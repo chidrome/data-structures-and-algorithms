ad01d47f3585e151a43c9fe2ef89046be964c090
chidrome <chidrome@gmail.com>
2019-04-22 22:28:54 -0700

    switched challenge 7 to be a switch statement. (get it? lol)

--- code-challenges/for-each/challenges-01.test.js
+++ code-challenges/for-each/challenges-01.test.js
@@ -156,14 +156,18 @@ const fizzbuzz = (arr) => {
   // Solution code here...
   let output = [];
   arr.forEach((num) => {
-    if(num % 3 === 0 && num % 5 === 0){
-      output.push('Fizz Buzz');
-    } else if(num % 3 === 0){
-      output.push('Fizz');
-    } else if(num % 5 === 0){
-      output.push('Buzz');
-    } else {
-    output.push(num);
+    switch(true) {
+      case num % 3 === 0 && num % 5 === 0:
+        output.push('Fizz Buzz');
+        break;
+      case num % 3 === 0:
+        output.push('Fizz');
+        break;
+      case num % 5 === 0:
+        output.push('Buzz');
+        break;
+      default:
+        output.push(num)
     }
   })
   return output;
