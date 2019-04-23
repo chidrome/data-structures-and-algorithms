42a693407d76f5dfbbbd42da1b21fb566d5808c8
chidrome <chidrome@gmail.com>
2019-04-22 22:09:40 -0700

    challenge 6 completed

--- code-challenges/for-each/challenges-01.test.js
+++ code-challenges/for-each/challenges-01.test.js
@@ -129,6 +129,13 @@ This function should use forEach to populate your grocery list based on the stor
 
 const createList = (availableItems) => {
   // Solution code here...
+  let groceryList = [];
+  availableItems.forEach((i) => {
+    if(i.available) {
+      groceryList.push(i.name);
+    }
+  })
+  return groceryList;
 };
 
 /* ------------------------------------------------------------------------------------------------
